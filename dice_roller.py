8ac05e54d1da4898ef63318a4dc0eb9f812bb65e
LavaLi0n <80548874+LavaLi0n@users.noreply.github.com>
2021-03-13 17:18:24 +0000

    Dice roller now rolls twice to get 2 numbers

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,10 @@
 import random
 def main():
   
-  roll = random.randint(1,6)
-  print(f'You rolled a {roll}')
+  dice-rolls = 2
+  for i in range(0,dice_rolls):
+    roll = random.randint(1,6)
+    print(f'You rolled a {roll}')
   
 
 if __name__== "__main__":
